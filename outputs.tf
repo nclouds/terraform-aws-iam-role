2a4ec7e80e53d193047c1fda7b8c069f158e76d9
Carlos Rodriguez <carlosrod.furlan@gmail.com>
2020-06-03 11:24:37 -0600

    standarize output format for all modules/examples

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,6 @@
-output "arn" {
-  value = aws_iam_role.role.arn
-}
-
-output "id" {
-  value = aws_iam_role.role.id
-}
-
-output "name" {
-  value = aws_iam_role.role.name
-}
-
-output "unique_id" {
-  value = aws_iam_role.role.unique_id
-}
-
-output "profile_arn" {
-  value = aws_iam_instance_profile.profile.arn
-}
-
-output "profile_id" {
-  value = aws_iam_instance_profile.profile.id
+output "output" {
+  value = {
+    instance_profile = aws_iam_instance_profile.profile
+    role             = aws_iam_role.role
+  }
 }
