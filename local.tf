32c47e0ca488dbd727c1662d5e98d9c8b092c0d7
Carlos Rodriguez <carlosrod.furlan@gmail.com>
2021-04-20 15:49:06 -0600

    reference as an array

--- local.tf
+++ local.tf
@@ -4,5 +4,5 @@ locals {
     replace(url, "https://", "")
   ]
 
-  assume_role_policy = length(var.oidc_fully_qualified_subjects) > 0 ? data.aws_iam_policy_document.oidc.json : data.aws_iam_policy_document.default.json
+  assume_role_policy = length(var.oidc_fully_qualified_subjects) > 0 ? data.aws_iam_policy_document.oidc.0.json : data.aws_iam_policy_document.default.0.json
 }
