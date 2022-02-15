b0005abe727d9355ce04d9e47b0f07d9ab4ceacf
carlosrodf <carlosrod.furlan@gmail.com>
2022-02-15 16:19:33 -0600

    feat: add policy attachments to module output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "output" {
   value = {
-    instance_profile = aws_iam_instance_profile.profile
-    role             = aws_iam_role.role
+    policy_attachments = aws_iam_role_policy_attachment.attach
+    instance_profile   = aws_iam_instance_profile.profile
+    role               = aws_iam_role.role
   }
 }
